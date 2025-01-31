2c89c3cb596a1e26cd315d8d8bfb21f1f2fff938
TilakSE <tilakelango29@gmail.com>
2025-01-31 11:57:52 +0530

    Initial commit

---
+++ src/main/java/com/mphasis/MyDemoProject/App.java
@@ -0,0 +1,21 @@
+package com.mphasis.MyDemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data) {
+		System.out.print("Data"+ data); //First commit
+		
+		if (data.equals("Tilak")) {		 //Second commit
+			System.out.println("Welcome");
+		}
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+       
+    }
+}
